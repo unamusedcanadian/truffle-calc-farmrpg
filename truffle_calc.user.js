2cf2909772c2ca02f04b88b0426a362dc1f61de8
Aidan Lee <aidanjlee@distantland.ca>
2025-10-30 22:05:24 -0400

    Minimum Viable Product achieved. Ready for open beta release.

--- truffle_calc.user.js
+++ truffle_calc.user.js
@@ -1,25 +1,23 @@
 // ==UserScript==
 // @name Truffle Calculator
 // @namespace unamusedcanadian
-// @version 0.1.5
+// @version 0.3.1
 // @description Determines approximate range of Truffles user can expect at daily reset.
 // @match https://farmrpg.com/*
 // @grant GM.getValue
 // @grant GM.setValue
-// @grant GM.listValues
-// @grant GM.deleteValue
 // @icon https://www.google.com/s2/favicons?sz=64&domain=farmrpg.com
 // @author unamusedcanadian
 // ==/UserScript==
 
-// Constant variables
-const DEBUG = true;
-const BENCHMARK = true;
+// TODO: Fix tab indentation
 
+// Constant variables
 const MAX_PIGS = 500;
 const W_TRUFFLE_DROP = 1/100;
 const B_TRUFFLE_DROP = 1/300;
 const CHANCE_CUTOFF = 1/10000;
+const FLT_TRUNCATE = 3;
 
 const FEED_BTN_CLASSES = '.item-link.close-panel.feedpigbtn';
 const FEED_ALL_BTN_CLASSES = '.item-link.close-panel.feedallbtn';
@@ -28,31 +26,26 @@ const BACON_BTN_CLASSES = '.item-link.close-panel.baconbtn';
 // ask someone who does have it for the last HTML class
 const BACON_ALL_BTN_CLASSES = '.item-link.close-panel.'; 
 
-// Global mutable variables
-var pigStats = []; // Attempt GM memory access
-var wtc = []; // Attempt GM memory access
-var btc = []; // Attempt GM memory access
-
-// Make these only true if GM memory access failed
-
-// Run every time pig levels could have changed
-var parseNeeded = !pigStats.length; 
-// Run when pig levels are confirmed to have changed
-var calculateNeeded = !(wtc.length && btc.length);
+// Global mutable variables.
+const scriptMemory = {
+    pigData: [],
+    wTruffleChances: [],
+    bTruffleChances: [],
+    parseNeeded: true
+};
 
 console.log('Truffle Calculator loaded!'); 
 
-// TODO: Benchmark this
-if (DEBUG) {
-	const testDict = { 1: 55, 2: 34, 3: 107 };
-	dataStore(testDict, "test");
-	GM.setValue("ShootyMcShootface", 69);
-	GM.listValues().then((out) => { console.log(out); });
-	dataList("test").then((out) => { console.log(out); });
-
-	dataWipe("test").then(() => { console.log("delete test"); 		
-		GM.listValues().then((out) => { console.log(out); }); });
-}
+// Make pig levels only parse if GM memory access failed or does not exist
+GM.getValue("pigData", "")
+  .then((data) => {
+      if (data) {
+	  scriptMemory.pigData = arrayDecode(data);
+	  scriptMemory.wTruffleChances = calculateProbability(scriptMemory.pigData, W_TRUFFLE_DROP);
+	  scriptMemory.bTruffleChances = calculateProbability(scriptMemory.pigData, B_TRUFFLE_DROP);
+	  scriptMemory.parseNeeded = false;
+      }
+  });
 
 // Only element we want that has an ID is the fireworks node
 // fireworks node seems to be constantly loaded, which is very useful.
@@ -63,212 +56,146 @@ const pages = fworks.querySelector('.pages');
 // refresh when the back button is pressed. This bug is part of the
 // website itself, and is likely impossible to fix from my end.
 const loader = new MutationObserver((mutationList, observer) => {
-  for (mutation of mutationList) {
-		for (newNode of mutation.addedNodes) {
-      switch (newNode.getAttribute('data-page')) {
-        case "pigpen":
-          // TODO: Attach event listener to send all button
-          pigpenLoad(newNode);
-          break;
-        case "namepig":
-          namepigLoad(newNode);
-          break;
-      }
-    }
-  }
+    mutationList.forEach((mutation) => {
+	mutation.addedNodes.forEach((newNode) => {
+	    switch (newNode.getAttribute('data-page')) {
+	    case "pigpen":
+		// TODO: Attach event listener to send all button
+		pigpenLoad(newNode);
+		break;
+	    case "namepig":
+		namepigLoad(newNode);
+		break;
+	    } 	 
+	});
+    });
 });
 
 loader.observe(pages, { childList: true });
 
+// When the namepig menu loads
+// Only place other than pigpenLoad where global variables are accessed
+function namepigLoad(elm) {
+    if (feedBtn = elm.querySelector(FEED_BTN_CLASSES)) {
+	feedBtn.addEventListener("click", () => { scriptMemory.parseNeeded = true; });
+    }
+    if (baconBtn = elm.querySelector(BACON_BTN_CLASSES)) {
+	baconBtn.addEventListener("click", () => { scriptMemory.parseNeeded = true; });
+    }
+}
+
 // Run every time the pigpen page is loaded
+// Only place I access or modify global variables
 function pigpenLoad(elm) {
   if (feedAllBtn = elm.querySelector(FEED_ALL_BTN_CLASSES)) {
-  	feedAllBtn.addEventListener("click", () => { parseNeeded = true; });
+  	feedAllBtn.addEventListener("click", () => { scriptMemory.parseNeeded = true; });
   }
   
   // Send all btn code goes here I guess
   
-  if (parseNeeded) {
-    const startTime = performance.now();
-  	pigStats = pigPenParse(elm);
-    const endTime = performance.now();
-    console.log(`Call to pigPenParse took ${endTime - startTime} milliseconds`);
-    parseNeeded = false;
+  if (scriptMemory.parseNeeded) {
+  	scriptMemory.pigData = pigPenParse(elm);
+    scriptMemory.wTruffleChances = calculateProbability(scriptMemory.pigData, W_TRUFFLE_DROP);
+  	scriptMemory.bTruffleChances = calculateProbability(scriptMemory.pigData, B_TRUFFLE_DROP);
+    scriptMemory.parseNeeded = false;
+      GM.setValue("pigData", arrayEncode(scriptMemory.pigData));
   }
   
-  if (calculateNeeded) {
-    const startTime1 = performance.now();
-    wtc = calculateProbability(pigStats, W_TRUFFLE_DROP);
-    const endTime1 = performance.now();
-    console.log(`Call to calculateProbability took ${endTime1 - startTime1} miliseconds`);
-    const startTime2 = performance.now();
-    btc = calculateProbability(pigStats, B_TRUFFLE_DROP);
-    const endTime2 = performance.now();
-    console.log(`Call to calculateProbability took ${endTime2 - startTime2} miliseconds`);
-    calculateNeeded = false;
-  }
+    const truffleMenu = elm.querySelector('.row.no-gutter');
   
-  if (DEBUG) {
-    console.log(wtc);
-  	console.log(btc);
-  }
+    renderData(truffleMenu.firstElementChild, scriptMemory.wTruffleChances);
+    renderData(truffleMenu.lastElementChild, scriptMemory.bTruffleChances);
 }
 
+// TODO: Differentiate unfed and to-be-bacon pigs
 // Runs when pigs are fed or sent to slaughterhouse.
 // Might want to make this promise-based for error catching
-function pigPenParse(elm) {  
-  
-  const startTime = performance.now();
-  // (NOTE: test to see if naming a pig "Level " screws anything up
-  // RegExp powered mass murder (~45-52ms)
-  const levels = Uint8Array.from(
-  	elm.innerText
-		.replace(/.*\)\n/s,"") // Nukes all non-pig text
-		.replace(/\n*.*\nLevel /g," ") // Nukes all pig names 
-    .match(/[0-9]+/g) // Extracts array of remaining numbers
-  );
-  const endTime = performance.now();
-  console.log(`Parsing HTML took ${endTime - startTime} miliseconds`);
+function pigPenParse(elm) {
+    const startTime = performance.now();
+    // RegExp powered mass murder (~45-52ms for 500 pigs)
+    const levels = Uint8Array.from(
+	elm.innerText
+	    .replace(/.*\)\n/s,"") // Nukes all non-pig text
+	    .replace(/\n*.*\nLevel /g," ") // Nukes all pig names 
+	    .match(/[0-9]+/g) // Extracts array of remaining numbers
+    );
+    const endTime = performance.now();
+    console.log(`Parsing HTML took ${endTime - startTime} miliseconds`);
 	
-  // Sorting pigs by level makes things simpler
-  levels.sort();
-  levels.reverse();
-  
-  console.log(levels);
-  
-  if (DEBUG) {
-    console.log(levels);
-  
-  	let sum = 0;
-  	for (num of levels) {
-  		sum += num; 
-  	}
-  	console.log('Sum of pig levels: ' + sum + '.'); 
-  }
-  
-  // Truncate unused memory
-  return levels;
+    // Sorting pigs by level makes things simpler later
+    return levels.sort();
 }
 
 // Calculates the rough probability of each truffle type being generated
 // Taken from https://en.wikipedia.org/wiki/Poisson_binomial_distribution
 function calculateProbability(levels, type) {
-	const probs = Array.from(levels).map(num => num * type);
-  let PMF = new Float64Array(1);
-  PMF[0] = 1;
+    const probs = Array.from(levels).map(num => num * type);
+    let PMF = new Float64Array(1);
+    PMF[0] = 1;
 
-  for (i = 1; i <= probs.length; ++i) {
-    let nextPMF = new Float64Array(i+1);
-    nextPMF[0] = (1-probs[i-1]) * PMF[0];
-    nextPMF[i] = probs[i-1] * PMF[i-1];
+    for (let i = 1; i <= probs.length; ++i) {
+	let nextPMF = new Float64Array(i+1);
+	nextPMF[0] = (1-probs[i-1]) * PMF[0];
+	nextPMF[i] = probs[i-1] * PMF[i-1];
     
-    for (j = 1; j < i; ++j) {
-    	nextPMF[j] = probs[i-1] * PMF[j-1] + (1-probs[i-1]) * PMF[j]
+	for (let j = 1; j < i; ++j) {
+	    nextPMF[j] = probs[i-1] * PMF[j-1] + (1-probs[i-1]) * PMF[j]
+	}
+    
+	PMF = nextPMF;
     }
-    
-    PMF = nextPMF;
-  }
   
-  // Any chance below CHANCE_CUTOFF is set to 0
-  return PMF.map(num => num > CHANCE_CUTOFF ? num : 0); 
+    // Any chance below CHANCE_CUTOFF is set to 0
+    return PMF.map(num => num > CHANCE_CUTOFF ? num : 0); 
 }
 
-// When the namepig menu loads
-function namepigLoad(elm) {
-	if (feedBtn = elm.querySelector(FEED_BTN_CLASSES)) {
-  	feedBtn.addEventListener("click", () => { parseNeeded = true; });
-  }
-  if (baconBtn = elm.querySelector(BACON_BTN_CLASSES)) {
-  	baconBtn.addEventListener("click", () => { parseNeeded = true; });
-  }
-}
-
-// Wrappers for GreaseMonkey API
-
-// Note: Arrays can be made from strings. Maybe store the arrays that way?
-// Would greatly reduce the number of calls we would need to make to
-//  the Greasemonkey API
-
-// TODO: Transition to string storage
-
-// Get all keys with a certain id
-function dataList(id) {
- 	return new Promise((resolve, reject) => {
-    GM.listValues()
-    	.then((data) => {
-      	// Might be able to simplify this function with more RegExp
-      	let index = 0;
-      	// This is vulnerable to usage of dashes in id names.
-        // Otherwise stable
-      	const regex = new RegExp(`${id}-`);
-      	const ret = data.map((str) => {
-        	if (str.match(regex)) { 
-            index++;
-            return str;
-          }
-        });
-        
-        // Move undefined to end of array and "delete" them
-      	ret.sort();
-      	ret.length = index;
-        
-        resolve(ret);
-    	})
-    	.catch((error) => { reject(error); });
-  }); 
+// Function to render the graphs
+// Graphics are placeholder right now
+// TODO: Learn how to make actually decent looking HTML
+function renderData(elm, data) {
+    const fragment = document.createDocumentFragment();
+    data.forEach((chance, count) => {
+	if (chance > 0) {
+	    const newline = document.createElement('br');
+	    const text = document.createTextNode(
+		`Chance of ${count} Truffles: ${floatToPercent(chance)}`
+	    );
+	    fragment.appendChild(newline);
+	    fragment.appendChild(text);
+	}
+    });
+    elm.appendChild(fragment);
 }
 
 
-// Store input dictonary under an id
-function dataStore(dict, id) {
-  return Promise.all(Object.entries(dict).map(([key, value]) => {
-    return GM.setValue(id + '-' + key, value); 
-  }));
+// Turns array into dictonary string for more space efficient storage
+function arrayEncode(arr) {
+    const compress = {};
+    arr.forEach((num) => {
+	compress[num] ? compress[num]++ : compress[num] = 1;
+    });
+    return JSON.stringify(compress);
 }
 
-/*
-// Retreive dictionary from id
-// Another function will expand it
-function dataGet(id) {
-	return new Promise((resolve, reject) => {
-		dataList(id)
-    	.then((data) => {
-      	const regex = new RegExp(`${id}-`);
-      	Promise.all(() => {
-        	let promList = []
-          for (entry of data) promList.push(GM.getValue(entry)) 
-        	return promList; })
-      		.then(() => {})
-      		.catch((error) => { reject(error); });
-      
-      	let lorem = data.map((str) => { 
-          return str.replace(regex,''); 
-        });
-      	
-    	}
-      .catch((error) => { reject(error); }});
-  });
-}
-*/
+// Turns dictonary string into array for usage in the script
+function arrayDecode(str) {
+    const compress = JSON.parse(str);
+    const keys = Object.keys(compress).map(c => parseInt(c));
+    const values = Object.values(compress);
+    const ret = [];
 
-// Deletes all key-value pairs with a certain id
-// TODO: Clean up spaghetti code
-function dataWipe(id) {
-  return new Promise((resolve, reject) => {
-  	dataList(id)
-  	.then((data) => {
-    	Promise.all(data.map((str) => {
-				return GM.deleteValue(str); 
-      }))
-      .then(() => { resolve(); })
-      .catch((error) => { reject() });
-  	})
-  	.catch((error) => { reject(); });
-  });
+    keys.forEach((k, i) => { 
+	ret.push(new Array(values[i]).fill(k));
+    });
+
+    return new Uint8Array(ret.flat());
 }
 
-// Remove id from all strings
-function idCleanse(arr, id) {
-  const regex = new RegExp(`${id}-`);
-	return arr.map((str) => { str.replace(regex, '') }); 
+// Formats floats as specified in config
+function floatToPercent(num) {
+    const regex = new RegExp(`(\\..{${FLT_TRUNCATE}}).*`);
+    const base = (num*100).toString()
+	  .replace(regex,'$1')
+	  .replace(/\.$/,'');
+    return base + '%';
 }
