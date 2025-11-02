4527d8503543295f469d693f729240c2f17f9a23
Aidan Lee <aidanjlee@distantland.ca>
2025-11-01 23:40:34 -0400

    Begun prototyping what the chart will look like

--- truffle_calc.user.js
+++ truffle_calc.user.js
@@ -10,39 +10,37 @@
 // @author unamusedcanadian
 // ==/UserScript==
 
-// Values that can be safely configured to alter script behaviour
+// Values that can be safely configured (within reason) to alter script behaviour
 const CONFIG = Object.freeze({
-    MAX_PIGS: 500,
+    MAX_PIGS: 500, // Keep at or above 0
+    CHANCE_CUTOFF: 1/10000,
+    FLT_TRUNCATE: 3, // Keep at or above 0
     W_TRUFFLE_DROP: 1/100,
     B_TRUFFLE_DROP: 1/300,
-    CHANCE_CUTOFF: 1/10000,
-    FLT_TRUNCATE: 3,
 });
 
-// Magic values are bad
-const BTN_CLASS = Object.freeze({
+// Prevents HTML classes from becoming magic values
+const HTML_CLASS = Object.freeze({
+    PIG_ELM: ".col-auto[style='line-height:14px;padding-bottom:12px;']",
     FEED_PIG: '.item-link.close-panel.feedpigbtn',
     FEED_ALL: '.item-link.close-panel.feedallbtn',
     BACON_PIG: '.item-link.close-panel.baconbtn',
-    // I am not buying send-o-matic 1 or 2 just to make this button work- 
-    // ask someone who does have it for the last HTML class
-    BACON_ALL: '.item-link.close-panel',
+    BACON_ALL: '.item-link.close-panel.massbtn',
     STOP_BACON: '.button.btngreen.stoppigsbtn',
 });
 
-// Global mutable variables
+// Global mutable variables- read/mutate these only when absolutely necessary
 const scriptMemory = {
     pigData: [],
     wTruffleChances: [],
     bTruffleChances: [],
-    parseNeeded: true
+    parseNeeded: true,
 };
 
-console.log('Truffle Calculator loaded!'); 
+console.log('Truffle Calculator loaded!');
 
 // Make pig levels only parse if GM memory access failed or does not exist
-GM.getValue("pigData", "")
-  .then((data) => {
+GM.getValue("pigData", "").then((data) => {
     if (data) {
         scriptMemory.pigData = arrayDecode(data);
         scriptMemory.wTruffleChances = calculateProbability(scriptMemory.pigData, CONFIG.W_TRUFFLE_DROP);
@@ -61,47 +59,39 @@ const pages = fworks.querySelector('.pages');
 // website itself, and is likely impossible to fix from my end.
 const loader = new MutationObserver((mutationList, observer) => {
     mutationList.forEach((mutation) => {
-        mutation.addedNodes.forEach((newNode) => {
-            switch (newNode.getAttribute('data-page')) {
-              case "pigpen":
+    mutation.addedNodes.forEach((newNode) => {
+        switch (newNode.getAttribute('data-page')) {
+            case "pigpen":
                 pigpenLoad(newNode);
                 break;
-              case "namepig":
-		namepigLoad(newNode);
+            case "namepig":
+                namepigLoad(newNode);
                 break;
-            } 	 
-        });
+        }
+    });
     });
 });
 
 loader.observe(pages, { childList: true });
 
+// Adds an event listener to element that triggers a re-parse of the pig pen
+function setupEventListenerIfExists(parent, childClass) {
+    if (child = parent.querySelector(childClass))
+        child.addEventListener("click", () => { scriptMemory.parseNeeded = true; });
+}
+
 // When the namepig menu loads
-// Only place other than pigpenLoad where global variables are accessed
 function namepigLoad(elm) {
-    if (feedBtn = elm.querySelector(BTN_CLASS.FEED_PIG)) {
-        feedBtn.addEventListener("click", () => { scriptMemory.parseNeeded = true; });
-    }
-    if (baconBtn = elm.querySelector(BTN_CLASS.BACON_PIG)) {
-        baconBtn.addEventListener("click", () => { scriptMemory.parseNeeded = true; });
-    }
+    setupEventListenerIfExists(elm, HTML_CLASS.FEED_PIG);
+    setupEventListenerIfExists(elm, HTML_CLASS.BACON_PIG);
 }
 
 // Run every time the pigpen page is loaded
-// Only place I access or modify global variables
 function pigpenLoad(elm) {
-    if (feedAllBtn = elm.querySelector(BTN_CLASS.FEED_ALL)) {
-        feedAllBtn.addEventListener("click", () => { scriptMemory.parseNeeded = true; });
-    }
-    /* Inactive until I find the classes of the send-o-matic button
-    if (sendAllBtn = elm.querySelector(BTN_CLASS.SEND_ALL)) {
-        sendAllBtn.addEventListener("click", () => { scriptMemory.parseNeeded = true; });
-    }
-    */  
-    if (stopPigsBtn = elm.querySelector(BTN_CLASS.STOP_BACON)) {
-        stopPigsBtn.addEventListener("click", () => { scriptMemory.parseNeeded = true; }); 
-    }
-  
+    setupEventListenerIfExists(elm, HTML_CLASS.FEED_ALL);
+    setupEventListenerIfExists(elm, HTML_CLASS.SEND_ALL);
+    setupEventListenerIfExists(elm, HTML_CLASS.STOP_BACON);
+
     if (scriptMemory.parseNeeded) {
         scriptMemory.pigData = pigpenParse(elm);
         scriptMemory.wTruffleChances = calculateProbability(scriptMemory.pigData, CONFIG.W_TRUFFLE_DROP);
@@ -109,10 +99,10 @@ function pigpenLoad(elm) {
         scriptMemory.parseNeeded = false;
         GM.setValue("pigData", arrayEncode(scriptMemory.pigData));
     }
-  
+
     // TODO: Make the rendering implementation more stable
     const truffleMenu = elm.querySelector('.row.no-gutter');
-  
+
     renderData(truffleMenu.firstElementChild, scriptMemory.wTruffleChances);
     renderData(truffleMenu.lastElementChild, scriptMemory.bTruffleChances);
 }
@@ -121,36 +111,35 @@ function pigpenLoad(elm) {
 // Returns an array with the levels of all fed and not-in-mortal-peril pigs
 function pigpenParse(elm) {
     console.time('pigpenParse');
-  
-    const pigList = elm.querySelectorAll(".col-auto\
-[style='line-height:14px;padding-bottom:12px;']");
-  
-    const accumulator = new Array(CONFIG.MAX_PIGS);
+
+    const pigList = elm.querySelectorAll(HTML_CLASS.PIG_ELM);
+
+    const levels = new Uint8Array(CONFIG.MAX_PIGS);
     let index = 0;
     pigList.forEach(pig => {
         const pigHTML = pig.innerHTML;
         // unfed pig
-        if (pigHTML.match(/color:red/)) return; 
+        if (pigHTML.match(/color:red/)) return;
         // to-be-bacon pig
         else if (pigHTML.match(/text-decoration: line-through;/)) return;
-        // catch any other edge cases
-        else if (num = parseInt(pig.innerText.slice(-2))) 
-            accumulator[index++] = num; 
-    }); 
-    accumulator.length = index;
-    const levels = Uint8Array.from(accumulator);
+        // catch any other edge cases like those weird trailing empty nodes
+        else if (num = parseInt(pig.innerText.slice(-2)))
+            levels[index++] = num;
+    });
+    levels.sort();
 
     console.timeEnd('pigpenParse');
-  
-    return levels.sort();
+
+    return levels.subarray(CONFIG.MAX_PIGS - index);
 }
 
 // Calculates the rough probability of each truffle type being generated
 // Taken from https://en.wikipedia.org/wiki/Poisson_binomial_distribution
 function calculateProbability(levels, type) {
     console.time('calculateProbability');
-    // TODO: Replace Array.from(levels) with just levels.map() if map() doesn't mutate array
-    const probs = Array.from(levels).map(num => num * type);
+
+    // Doing a .map on levels without casting it to Float causes logic errors
+    const probs = Float64Array.from(levels).map(num => num * type);
     let PMF = new Float64Array(1);
     PMF[0] = 1;
 
@@ -158,18 +147,18 @@ function calculateProbability(levels, type) {
         let nextPMF = new Float64Array(i+1);
         nextPMF[0] = (1-probs[i-1]) * PMF[0];
         nextPMF[i] = probs[i-1] * PMF[i-1];
-    
+
         for (let j = 1; j < i; ++j) {
             nextPMF[j] = probs[i-1] * PMF[j-1] + (1-probs[i-1]) * PMF[j]
         }
-    
+
         PMF = nextPMF;
     }
-  
+
     console.timeEnd('calculateProbability');
-  
+
     // Any chance below CHANCE_CUTOFF is set to 0
-    return PMF.map(num => num > CONFIG.CHANCE_CUTOFF ? num : 0); 
+    return PMF.map(num => num > CONFIG.CHANCE_CUTOFF ? num : 0);
 }
 
 // Function to render the graphs
@@ -178,6 +167,7 @@ function calculateProbability(levels, type) {
 // TODO: Show the sum of all Truffle-making pig levels
 function renderData(elm, data) {
     const fragment = document.createDocumentFragment();
+
     data.forEach((chance, count) => {
         if (chance > 0) {
             const newline = document.createElement('br');
@@ -207,7 +197,8 @@ function arrayDecode(str) {
     const values = Object.values(compress);
     const ret = [];
 
-    keys.forEach((k, i) => { 
+    // Might be able to replace this with a .reduce() method call
+    keys.forEach((k, i) => {
         ret.push(new Array(values[i]).fill(k));
     });
 
@@ -218,7 +209,7 @@ function arrayDecode(str) {
 function floatToPercent(num) {
     const regex = new RegExp(`(\\..{${CONFIG.FLT_TRUNCATE}}).*`);
     const base = (num*100).toString()
-      .replace(regex,'$1')
-      .replace(/\.$/,'');
+        .replace(regex,'$1')
+        .replace(/\.$/,'');
     return `${base}%`;
 }
