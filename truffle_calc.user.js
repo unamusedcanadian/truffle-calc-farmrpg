378deb4679a234ea54094c1ae4e3b1174c6406fa
Aidan Lee <aidanjlee@distantland.ca>
2025-11-05 20:36:24 -0500

    Preparing for full release.
    COMPLETE:
    - DOM manipulation code updated
    - Pig parser refactored
    - Greasemonkey value storage functionality removed
    - Frontend UI overhauled and given it's own section of the webpage
    - Probability calculations combined with pig parser
    - README Written
    - Dynamic Y-Axis scaling on graphs
    - Licence added
    - DOM manipulation code stabilized

--- truffle_calc.user.js
+++ truffle_calc.user.js
@@ -1,22 +1,59 @@
 // ==UserScript==
 // @name Truffle Calculator
 // @namespace unamusedcanadian
-// @version 0.5.1
+// @version 1.0.1
 // @description Determines approximate range of Truffles user can expect at daily reset.
 // @match https://farmrpg.com/*
-// @grant GM.getValue
-// @grant GM.setValue
 // @icon https://www.google.com/s2/favicons?sz=64&domain=farmrpg.com
 // @author unamusedcanadian
 // ==/UserScript==
 
 // Values that can be safely configured (within reason) to alter script behaviour
+// Keep all values within a range of 0 to 1 inclusive.
 const CONFIG = Object.freeze({
-    MAX_PIGS: 500, // Keep at or above 0
     CHANCE_CUTOFF: 1/1000,
-    FLT_TRUNCATE: 3, // Keep at or above 0
-    W_TRUFFLE_DROP: 1/100,
-    B_TRUFFLE_DROP: 1/300,
+    WHITE_TRUFFLE_DROP: 1/100,
+    BLACK_TRUFFLE_DROP: 1/300,
+});
+
+// For more advanced users who want to change the colours of the graphs
+const WHITE_TRUFFLE_OPTIONS = Object.freeze({
+    CAPTION: 'White Truffle Stats',
+    COLOUR_PROFILE: 'wtruf-col',
+    IDLE_BAR_COLOUR: 'beige',
+    HOVER_BAR_COLOUR: 'grey',
+});
+const BLACK_TRUFFLE_OPTIONS = Object.freeze({
+    CAPTION: 'Black Truffle Stats',
+    COLOUR_PROFILE: 'btruf-col',
+    IDLE_BAR_COLOUR: 'brown',
+    HOVER_BAR_COLOUR: 'grey',
+});
+
+// Readable formatting can be found in the style.css file on GitHub
+// Highly recommend that any attempt to refactor this be done with that first
+const CSS_STYLE = Object.freeze({
+    STYLE: `.chart { --caption-area: 1.5rem; --footer-area: 1.5rem;\
+--ui-colour: #eee; background-color: transparent; box-sizing: border-box;\
+container-type: inline-size; display: block; & * { box-sizing: border-box; }\
+table { display: grid; grid-template-rows: var(--caption-area) minmax(0, 1fr)\
+var(--footer-area); height: 100%; } caption { grid-area: 1 / 1 / 2 / 2;\
+display: block; font-size: 1rem; } thead { align-items: end; display: grid;\
+grid-area: 1 / 1 / 3 / 2; grid-auto-rows: minmax(0, 1fr); grid-template-rows: \
+var(--caption-area); tr { display: contents; } th { border-block-end: 1px \
+solid var(--ui-colour); display: inline grid; font-size: 0.75rem; text-align: start;\
+} } tbody { container-type: size; display: grid; grid-auto-flow: column;\
+grid-auto-columns: minmax(0, 1fr); grid-area: 2 / 1 / 4 / 2; padding-inline: \
+1.5rem 0; tr { align-items: end; display: grid; grid-template-rows: 1fr \
+var(--footer-area); } tr:hover { th { display: inline grid; } .wtruf-col {\
+background-color: ${WHITE_TRUFFLE_OPTIONS.HOVER_BAR_COLOUR}; } .btruf-col {\
+background-color: ${BLACK_TRUFFLE_OPTIONS.HOVER_BAR_COLOUR}; } } th {\
+border-block-start: 2px solid var(--ui-colour); display: none; font-size: 1rem;\
+grid-area: 2 / 1 / 3 / 2; height: var(--footer-area); min-width: 0;\
+overflow: visible; place-content: center; text-align: center; &:has(+ td:hover) \
+{ display: inline grid; } } td { grid-area: 1 / 1 / 2 / 2; } } } .wtruf-col {\
+background-color: ${WHITE_TRUFFLE_OPTIONS.IDLE_BAR_COLOUR}; } .btruf-col { \
+background-color: ${BLACK_TRUFFLE_OPTIONS.IDLE_BAR_COLOUR}; }`
 });
 
 // Prevents HTML classes from becoming magic values
@@ -31,35 +68,19 @@ const HTML_CLASS = Object.freeze({
 
 // Global mutable variables- read/mutate these only when absolutely necessary
 const scriptMemory = {
-    pigData: [],
-    wTruffleChances: [],
-    bTruffleChances: [],
+    frontendHTML: "",
     parseNeeded: true,
 };
 
+// Confirms that the script loaded. If you don't see this something messed up
 console.log('Truffle Calculator loaded!');
 
-// Make pig levels only parse if GM memory access failed or does not exist
-GM.getValue("pigData", "").then((data) => {
-    if (data) {
-        scriptMemory.pigData = arrayDecode(data);
-        scriptMemory.wTruffleChances = calculateProbability(scriptMemory.pigData, CONFIG.W_TRUFFLE_DROP);
-        scriptMemory.bTruffleChances = calculateProbability(scriptMemory.pigData, CONFIG.B_TRUFFLE_DROP);
-        scriptMemory.parseNeeded = false;
-    }
-});
-
-// Only element we want that has an ID is the fireworks node
-// fireworks node seems to be constantly loaded, which is very useful.
-const fworks = document.getElementById('fireworks');
-const pages = fworks.querySelector('.pages');
-
 // Sometimes, even when the pigpen is still in memory/background, it will
-// refresh when the back button is pressed. This bug is part of the
+// refresh when the back button is pressed. This bug (feature?) is part of the
 // website itself, and is likely impossible to fix from my end.
-const loader = new MutationObserver((mutationList, observer) => {
-    mutationList.forEach((mutation) => {
-    mutation.addedNodes.forEach((newNode) => {
+const loader = new MutationObserver(mutationList => {
+    mutationList.forEach(mutation => {
+    mutation.addedNodes.forEach(newNode => {
         switch (newNode.getAttribute('data-page')) {
             case "pigpen":
                 pigpenLoad(newNode);
@@ -72,6 +93,8 @@ const loader = new MutationObserver((mutationList, observer) => {
     });
 });
 
+// Look for page updates
+const pages = document.getElementById('fireworks').querySelector('.pages');
 loader.observe(pages, { childList: true });
 
 // Adds an event listener to element that triggers a re-parse of the pig pen
@@ -82,29 +105,25 @@ function setupEventListenerIfExists(parent, childClass) {
 
 // When the namepig menu loads
 function namepigLoad(elm) {
+    // TODO: Replace this with an event listener on the parent (elm)
     setupEventListenerIfExists(elm, HTML_CLASS.FEED_PIG);
     setupEventListenerIfExists(elm, HTML_CLASS.BACON_PIG);
 }
 
 // Run every time the pigpen page is loaded
 function pigpenLoad(elm) {
+    // TODO: Replace this with an event listener on the parent (elm)
     setupEventListenerIfExists(elm, HTML_CLASS.FEED_ALL);
     setupEventListenerIfExists(elm, HTML_CLASS.SEND_ALL);
     setupEventListenerIfExists(elm, HTML_CLASS.STOP_BACON);
 
     if (scriptMemory.parseNeeded) {
-        scriptMemory.pigData = pigpenParse(elm);
-        scriptMemory.wTruffleChances = calculateProbability(scriptMemory.pigData, CONFIG.W_TRUFFLE_DROP);
-        scriptMemory.bTruffleChances = calculateProbability(scriptMemory.pigData, CONFIG.B_TRUFFLE_DROP);
+        let parseData = pigpenParse(elm);
+        scriptMemory.frontendHTML = createFrontend(parseData);
         scriptMemory.parseNeeded = false;
-        GM.setValue("pigData", arrayEncode(scriptMemory.pigData));
     }
 
-    // TODO: Make the rendering implementation more stable
-    const truffleMenu = elm.querySelector('.row.no-gutter');
-
-    renderData(truffleMenu.firstElementChild, scriptMemory.wTruffleChances);
-    renderData(truffleMenu.lastElementChild, scriptMemory.bTruffleChances);
+    renderFrontend(elm, scriptMemory.frontendHTML);
 }
 
 // Runs when pigs are fed or sent to slaughterhouse.
@@ -114,265 +133,139 @@ function pigpenParse(elm) {
 
     const pigList = elm.querySelectorAll(HTML_CLASS.PIG_ELM);
 
-    const levels = new Uint8Array(CONFIG.MAX_PIGS);
-    let index = 0;
+    let sum = 0;
+    let whiteTruffleChances = new Float64Array([1]);
+    let blackTruffleChances = new Float64Array([1]);
+
+    // TODO: make this function more efficient
     pigList.forEach(pig => {
         const pigHTML = pig.innerHTML;
-        // unfed pig
-        if (pigHTML.match(/color:red/)) return;
-        // to-be-bacon pig
-        else if (pigHTML.match(/text-decoration: line-through;/)) return;
-        // catch any other edge cases like those weird trailing empty nodes
-        else if (num = parseInt(pig.innerText.slice(-2)))
-            levels[index++] = num;
+        if (pigHTML.match(/color:red|text-decoration: line-through;/))
+            return; // unfed or to-be-bacon pig
+        else if (num = parseInt(pig.innerText.slice(-2))) {
+            sum += num;
+            whiteTruffleChances = calculateProbabilityParallel(
+                whiteTruffleChances,
+                num * CONFIG.WHITE_TRUFFLE_DROP
+            );
+            blackTruffleChances = calculateProbabilityParallel(
+                blackTruffleChances,
+                num * CONFIG.BLACK_TRUFFLE_DROP
+            );
+        }
     });
-    levels.sort();
 
     console.timeEnd('pigpenParse');
 
-    return levels.subarray(CONFIG.MAX_PIGS - index);
+    return Object.freeze({
+        totalPigLevels: sum,
+        whiteTruffleChances: whiteTruffleChances,
+        blackTruffleChances: blackTruffleChances,
+    });
 }
 
 // Calculates the rough probability of each truffle type being generated
 // Taken from https://en.wikipedia.org/wiki/Poisson_binomial_distribution
-function calculateProbability(levels, type) {
-    console.time('calculateProbability');
+function calculateProbabilityParallel(PMF, prob) {
+    const size = PMF.length;
+    let nextPMF = new Float64Array(size+1);
 
-    // Doing a .map on levels without casting it to Float causes logic errors
-    const probs = Float64Array.from(levels).map(num => num * type);
-    let PMF = new Float64Array(1);
-    PMF[0] = 1;
+    nextPMF[0] = (1-prob) * PMF[0];
+    nextPMF[size] = prob * PMF[size-1];
 
-    for (let i = 1; i <= probs.length; ++i) {
-        let nextPMF = new Float64Array(i+1);
-        nextPMF[0] = (1-probs[i-1]) * PMF[0];
-        nextPMF[i] = probs[i-1] * PMF[i-1];
+    for (let i = 1; i < size; ++i)
+        nextPMF[i] = prob * PMF[i-1] + (1-prob) * PMF[i];
 
-        for (let j = 1; j < i; ++j) {
-            nextPMF[j] = probs[i-1] * PMF[j-1] + (1-probs[i-1]) * PMF[j]
-        }
-
-        PMF = nextPMF;
-    }
-
-    console.timeEnd('calculateProbability');
-
-    // Any chance below CHANCE_CUTOFF is set to 0
-    return PMF.map(num => num > CONFIG.CHANCE_CUTOFF ? num : 0);
+    return nextPMF;
 }
 
-// Turns array into dictonary string for more space efficient storage
-function arrayEncode(arr) {
-    const compress = {};
-    arr.forEach((num) => {
-        compress[num] ? compress[num]++ : compress[num] = 1;
-    });
-    return JSON.stringify(compress);
+// Ensures the content is placed in an appropriate space
+// TODO: Tidy up code by reducing calls to .match
+function renderFrontend(elm, frontendHTML) {
+    console.time('render');
+
+    const render = document.createElement('template');
+    render.innerHTML = frontendHTML;
+
+    // If user unlocked truffles, add this under that area.
+    // Else, add it right before the pigs section.
+    const anchorList = Array.from(elm.querySelectorAll('.content-block-title'));
+    const anchor = anchorList.find(
+        title => title.innerText.match(/Truffles|Pigs/i)
+    );
+
+    if (anchor.innerText.match(/Truffles/i))
+        anchor.parentElement.insertBefore(
+            render.content,
+            anchor.nextElementSibling.nextElementSibling
+        );
+    else if (anchor.innerText.match(/Pigs/i))
+        anchor.parentElement.insertBefore(
+            render.content,
+            anchor
+        );
+    else
+        console.log("ERROR: No valid anchor detected. Rendering cancelled.");
+
+    console.timeEnd('render');
 }
 
-// Turns dictonary string into array for usage in the script
-function arrayDecode(str) {
-    const compress = JSON.parse(str);
-    const keys = Object.keys(compress).map(c => parseInt(c));
-    const values = Object.values(compress);
-    const ret = [];
-
-    // Might be able to replace this with a .reduce() method call
-    keys.forEach((k, i) => {
-        ret.push(new Array(values[i]).fill(k));
-    });
-
-    // Probably a better way to do this
-    return new Uint8Array(ret.flat());
-}
-
-// Formats floats as specified in config
-function floatToPercent(num) {
-    const regex = new RegExp(`(\\..{${CONFIG.FLT_TRUNCATE}}).*`);
-    const base = (num*100).toString()
-        .replace(regex,'$1')
-        .replace(/\.$/,'');
-    return `${base}%`;
-}
-
-// ############################## //
-// Abandon all hope, ye who enter //
-// ############################## //
+// Create the custom website section of the pigpen
+// TODO: Show the range of truffles without hovering
+// TODO: Modularize this giant text block to make modification easier
+const createFrontend = data =>
+    `<div class="content-block-title">Truffle Calculations</div>\
+<div class="row no-gutter" style="margin-bottom:0"><style>${CSS_STYLE.STYLE}\
+</style>${createGraph(WHITE_TRUFFLE_OPTIONS, data.whiteTruffleChances)}\
+${createGraph(BLACK_TRUFFLE_OPTIONS, data.blackTruffleChances)}</div>\
+<div class="card"><div class="card-content"><div class="card-content-inner">\
+Tip: White Truffles are best sold at <strong>190m-200m</strong> silver each.<br>\
+Tip: Black Truffles are best sold at <strong>590m-600m</strong> silver each.<br><br>\
+Hover your mouse over the graph to see the quantities of truffles.<br><br>\
+Sum of all truffle-producing pig levels: <strong>${data.totalPigLevels}</strong>.\
+<br><br>Currently not showing any quantity with chances under <strong>\
+${100*CONFIG.CHANCE_CUTOFF}%</strong>. This can be changed in the config.\
+</div></div></div>`;
 
 // Function to render the graphs
-// TODO: Show the sum of all Truffle-making pig levels
-// TODO: Make the code more readable and efficient
-// TODO: Make the graph generate a dynamic Y axis based on average rarity
-// TODO: Show the range of truffles
-// TODO: Maybe find a way to store the result in memory? Maybe?
-// TODO: Have White and Black Truffles have different bar colours
-function renderData(elm, data) {
-    console.time('renderData');
+function createGraph(options, chances) {
+    // This method is kinda hacky but I don't think there's a better way
+    const chartMax = Math.ceil(
+        100 * chances.reduce((a, b) =>
+            Math.max(a, b)
+        )
+    ) / 100;
 
-    const max = 0.15; // Currently useless
+    // To reduce the amount of times a new string has to be created
+    const chartHTML = [`<div class="col-auto chart"><table>\
+<caption>${options.CAPTION}</caption><thead><tr><th>${100*chartMax}%</th></tr>`];
 
-    const fragment = document.createDocumentFragment();
-
-    const style = document.createElement('style');
-    setStyle(style);
-    fragment.appendChild(style);
-
-    const chart = document.createElement('chart');
-    fragment.appendChild(chart);
-
-    const table = document.createElement('table');
-    chart.appendChild(table);
-
-    const caption = document.createElement('caption');
-    caption.appendChild(document.createTextNode('Chances'));
-    table.appendChild(caption);
-
-    const thead = document.createElement('thead');
-    table.appendChild(thead);
-
-    // Determine how to generate this later
-    yAxisGen(0, 15, 5, 1, thead);
-
-    const tbody = document.createElement('tbody');
-    table.appendChild(tbody);
-
-    // Generates main table
-    data.forEach((chance, count) => {
-        if (chance <= 0) return;
-
-        const tr = document.createElement('tr');
-        tbody.appendChild(tr);
-
-        const th = document.createElement('th');
-        const td = document.createElement('td');
-        // th.appendChild(document.createTextNode(count.toString()));
-        td.setAttribute("style", "height:" + (100 * chance/max) + "%");
-
-        // This order is very important- th must always come before td
-        tr.appendChild(th);
-        tr.appendChild(td);
-    });
-
-    elm.appendChild(fragment);
-    console.timeEnd('renderData');
-}
-
-// TODO: Make this more efficient and readable
-function yAxisGen(min, max, step, gap, head) {
-    const appendMe = [];
-
-    for (i = min; i <= max; i += step) {
-        const tr = document.createElement('tr');
-        const th = document.createElement('th');
-        th.appendChild(document.createTextNode(`${i}%`));
-        tr.appendChild(th);
-        appendMe.push(tr);
-
-        if (i < max)
-        for (j = 0; j < gap; ++j) {
-            const tr2 = document.createElement('tr');
-            const th2 = document.createElement('th');
-            tr2.appendChild(th2);
-            appendMe.push(tr2);
-        }
+    // Determine Y axis formatting
+    // If divisible by 3
+    if ((100*chartMax % 3) == 0) {
+    chartHTML.push(`<tr><th></th></tr><tr><th>${200*chartMax/3}%</th></tr>\
+    <tr><th></th></tr><tr><th>${100*chartMax/3}%</th></tr><tr><th></th></tr>`);
+    }
+    // If divisible by 2
+    else if ((100*chartMax & 1) == 0) {
+        chartHTML.push(`<tr><th></th></tr><tr><th></th></tr><tr>\
+<th>${50*chartMax}%</th></tr><tr><th></th></tr><tr><th></th></tr>`);
+    }
+    else {
+        chartHTML.push(`<tr><th></th></tr><tr><th></th></tr><tr><th></th></tr>\
+<tr><th></th></tr><tr><th></th></tr>`);
     }
 
-    appendMe.reverse();
+    chartHTML.push(`<tr><th>0%</th></tr></thead><tbody>`);
 
-    appendMe.forEach(part => {
-        head.appendChild(part);
+    // Creates the bars of the chart
+    chances.forEach((chance, count) => {
+        if (chance > CONFIG.CHANCE_CUTOFF)
+            chartHTML.push(`<tr><th>${count}</th><td class=\
+"${options.COLOUR_PROFILE}"style="height:${100*chance/chartMax}%"></td></tr>`);
     });
-}
 
-// The CSS that creates the entire chart
-// Will probably replace this with style objects instead of adding this to the DOM directly
-function setStyle(style) {
-    style.innerHTML = `
-    chart {
-        --caption-area: 1.5rem;
-        --footer-area: 1.5rem;
-        --ui-colour: white;
+    chartHTML.push(`</tbody></table></div>`);
 
-        display: block;
-        box-sizing: border-box;
-        container-type: inline-size;
-        background-color: transparent;
-
-        & * { box-sizing: border-box; }
-
-        table {
-            display: grid;
-            grid-template-rows: var(--caption-area) 1fr var(--footer-area);
-            height: 100%;
-            max-height: 225px;
-            max-width: 450px;
-        }
-
-        caption {
-            grid-area: 1 / 1 / 2 / 2;
-            display: block;
-            font-size: 1rem;
-        }
-
-        thead {
-            grid-area: 1 / 1 / 3 / 2;
-            align-items: end;
-            color: var(--ui-colour);
-            display: grid;
-            grid-template-rows: var(--caption-area);
-            grid-auto-rows: 1fr;
-
-            tr {
-                display: contents;
-            }
-
-            th {
-                font-size: 0.75rem;
-                font-weight: 600;
-                text-align: start;
-                border-block-end: 1px solid var(--ui-colour);
-                display: inline grid;
-            }
-        }
-
-        tbody {
-            grid-area: 2 / 1 / 4 / 2;
-            container-type: size;
-            display: grid;
-            grid-auto-flow: column;
-            grid-auto-columns: 1fr;
-            padding-inline: 1rem 0;
-
-            tr {
-                align-items: end;
-                display: grid;
-                overflow-inline: clip;
-                grid-template-rows: 1fr var(--footer-area);
-            }
-
-            th {
-                grid-area: 2 / 1 / 3 / 2;
-                color: var(--ui-colour);
-                border-block-start: 2px solid var(--ui-colour);
-                font-size: 1rem;
-                font-weight: 600;
-                height: var(--footer-area);
-                place-content: center;
-                display: none;
-
-                &:has(+ td:hover) {
-                    display: inline grid;
-                }
-            }
-
-            td {
-                grid-area: 1 / 1 / 2 / 2;
-                background: beige;
-
-                &:hover {
-                    background-color: blue;
-                }
-            }
-        }
-    }`;
+    return chartHTML.join("");
 }
